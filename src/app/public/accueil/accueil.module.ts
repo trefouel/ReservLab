05bd817b4591d589f6a22420b2523bcb68613fa8
Samson AKUMANI <samsakumani@gmail.com>
2022-10-24 19:21:46 +0000

    Update of routing but still have some bugs with the link of the nav

--- src/app/public/accueil/accueil.module.ts
+++ src/app/public/accueil/accueil.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccueilRoutingModule } from './accueil-routing.module';
+import { SharedPublicModule } from '../shared-public/shared-public.module';
+import { PageComponent } from './page/page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageComponent
+  ],
   imports: [
     CommonModule,
-    AccueilRoutingModule
+    AccueilRoutingModule,
+    SharedPublicModule
   ]
 })
 export class AccueilModule { }
--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './accueil/page/page.component';
 import { AuthComponent } from './connexion/auth/auth.component';
 
 const routes: Routes = [
-  {path: 'connexion', component: AuthComponent}
+  {path: 'home', component: PageComponent},
+  {path: 'connexion', component: AuthComponent},
 ];
 
 @NgModule({
